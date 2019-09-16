8354b2a4dfb2fc4e095bda2150ac1b51ba8b5dbd
1064064 <boyangs@student.unimelb.edu.au>
2019-09-16 20:29:21 +1000

    P3 DOING

---
+++ src/Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -0,0 +1,21 @@
+package Longest_Substring_Without_Repeating_Characters;
+
+import java.util.LinkedList;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int output = 1;
+        for(int i=s.length();i>=1;i++){
+            LinkedList<String> list = getn(s,i);
+
+        }
+        return output;
+    }
+    public LinkedList<String> getn(String s,int n){
+        LinkedList<String> list =new LinkedList<String>();
+
+    }
+    public boolean hasRepeat(String s){
+
+    }
+}
