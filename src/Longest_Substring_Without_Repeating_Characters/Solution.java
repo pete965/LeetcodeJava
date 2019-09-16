72e98a33c7583a223cfc7749048c52355ad8e106
1064064 <boyangs@student.unimelb.edu.au>
2019-09-17 00:47:04 +1000

    p3 NEARLY

--- src/Longest_Substring_Without_Repeating_Characters/Solution.java
+++ src/Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -1,21 +1,49 @@
 package Longest_Substring_Without_Repeating_Characters;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int output = 1;
-        for(int i=s.length();i>=1;i++){
-            LinkedList<String> list = getn(s,i);
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int n = solution.lengthOfLongestSubstring("abcabcbb");
+        System.out.println(n);
+    }
 
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==0){
+            return 0;
         }
-        return output;
+        s=s.trim();
+        for(int i=s.length();i>1;i--){
+            System.out.println("Length:"+i);
+            LinkedList<String> list = getn(s,i);
+            for(String string:list){
+                System.out.println(string);
+                if(!hasRepeat(string)){
+                    return i;
+                }
+            }
+        }
+        return 1;
     }
     public LinkedList<String> getn(String s,int n){
         LinkedList<String> list =new LinkedList<String>();
-
+        for(int i=0;i<=s.length()-n;i++){
+            list.add(s.substring(i,i+n));
+        }
+        return list;
     }
     public boolean hasRepeat(String s){
-
+        HashMap<Character,Integer> hashMap =  new HashMap<Character,Integer>();
+        for(int i = 0;i<s.length();i++){
+            Character character = s.charAt(i);
+            if(hashMap.containsKey(character)){
+                return true;
+            }else{
+                hashMap.put(character,1);
+            }
+        }
+        return false;
     }
 }
