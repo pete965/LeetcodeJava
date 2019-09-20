2cf77af50530b7f924779f54cefcae0c6b68eb4b
1064064 <boyangs@student.unimelb.edu.au>
2019-09-20 13:21:30 +1000

    P9 completed

--- src/No_8_String_to_Integer/Solution.java
+++ src/No_8_String_to_Integer/Solution.java
@@ -1,4 +1,45 @@
 package No_8_String_to_Integer;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.myAtoi(" 1234"));
+    }
+    public int myAtoi(String str) {
+        int no = 0;
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        int length = str.length();
+        if (length==0){
+            return 0;
+        }
+        int i = 0;
+        while(str.charAt(i)==' '){
+            i++;
+            System.out.println("i="+i);
+        }
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        for(int j=0;j<10;j++){
+            number.add(j);
+            System.out.println(number);
+        }
+        System.out.println(i);
+        System.out.println(Integer.valueOf(str.charAt(i)));
+        System.out.println(Integer.valueOf('0'));
+        if (!number.contains(Integer.valueOf(str.charAt(i))-48)){
+            return 0;
+        }else {
+            while(number.contains(str.charAt(i))&&i<length){
+                System.out.println("Add a digit");
+                output.add(Character.getNumericValue(str.charAt(i)));
+                i++;
+            }
+        }
+        for (int j=output.size()-1;j>=0;j--){
+            no+=output.get(output.size()-1-j)*Math.pow(10,j);
+        }
+        return no;
+    }
 }
---
+++ src/No_9_Palindrome_Number/Solution.java
@@ -0,0 +1,24 @@
+package No_9_Palindrome_Number;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        boolean output = true;
+        String number=String.valueOf(x);
+
+        Stack<Character> stack=new Stack<>();
+        for (int i=0;i<number.length();i++){
+            stack.push(number.charAt(i));
+        }
+        int j=0;
+        while(!stack.isEmpty()){
+            Character pop = stack.pop();
+            if(number.charAt(j)!=pop){
+                output = false;
+            }
+            j++;
+        }
+        return output;
+    }
+}
