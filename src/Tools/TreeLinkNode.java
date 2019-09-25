f2c94717fe2dbc8985a2c925355caaf55bdd6887
1064064 <boyangs@student.unimelb.edu.au>
2019-09-25 16:25:51 +1000

    Sword 057 Completed

---
+++ src/Tools/TreeLinkNode.java
@@ -0,0 +1,14 @@
+package Tools;
+
+
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left = null;
+    public TreeLinkNode right = null;
+    public TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
+
