6fc902c8f673243d6c7e046ea7fe48096ab1cf8a
1064064 <boyangs@student.unimelb.edu.au>
2019-10-04 23:11:48 +1000

    Sword 011 Completed

--- src/Coding_Interview/Sword_Offer_011/Solution.java
+++ src/Coding_Interview/Sword_Offer_011/Solution.java
@@ -2,6 +2,11 @@ package Coding_Interview.Sword_Offer_011;
 
 public class Solution {
     public int NumberOf1(int n) {
-
+        int count=0;
+        while(n!=0){
+            count++;
+            n=n&(n-1);
+        }
+        return count;
     }
 }
