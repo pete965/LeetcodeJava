bcce3169ea8b54448e8f712b2fc2382c87948c2f
1064064 <boyangs@student.unimelb.edu.au>
2019-10-05 00:35:57 +1000

    Sword 013 Completed

--- src/Coding_Interview/Sword_Offer_010/Solution.java
+++ src/Coding_Interview/Sword_Offer_010/Solution.java
@@ -1,4 +1,18 @@
 package Coding_Interview.Sword_Offer_010;
 
 public class Solution {
+    public int RectCover(int target) {
+        if (target==1||target==0||target==2){
+            return target;
+        }
+        return calN(target);
+    }
+
+    private int calN(int target) {
+        if (target==1||target==2){
+            return target;
+        }else{
+            return calN(target-1) + calN(target-2);
+        }
+    }
 }
--- src/Coding_Interview/Sword_Offer_013/Solution.java
+++ src/Coding_Interview/Sword_Offer_013/Solution.java
@@ -6,10 +6,18 @@ public class Solution {
         if (length==0||length==1){
             return;
         }
-        int odd=0,even=1;
         for (int i=0;i<length;i++){
             if (array[i]%2==0){
-                even++;
+                for (int j=i+1;j<length;j++){
+                    if (array[j]%2==1){
+                        int temp=array[j];
+                        for (int k=j-1;k>=i;k--){
+                            array[k+1]=array[k];
+                        }
+                        array[i]=temp;
+                        break;
+                    }
+                }
             }
         }
     }
