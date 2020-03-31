5ca7dab21ca9348447e1663b88308c9d73b2e00a
1064064 <boyangs@student.unimelb.edu.au>
2020-03-31 15:28:27 +0800

    063_and_067_Completed

--- src/Coding_Interview/Sword_Offer_063/Solution.java
+++ src/Coding_Interview/Sword_Offer_063/Solution.java
@@ -1,8 +1,53 @@
 package Coding_Interview.Sword_Offer_063;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Map;
+
 /*
 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。
 如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
 我们使用Insert()方法读取数据流，使用GetMedian()方法获取当前读取数据的中位数。
  */
 public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{5,2,3,4,1,6,7,0,8};
+        for (int i=0;i<nums.length;i++){
+            solution.Insert(nums[i]);
+            Double res = solution.GetMedian();
+//            System.out.println("Insert:"+nums[i]);
+//            System.out.println(solution.GetMedian());
+        }
+    }
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    int length = 0;
+    public void Insert(Integer num) {
+        if (linkedList.size()==0){
+            linkedList.add(num);
+        }else{
+            int index = Collections.binarySearch(linkedList,num);
+            if (index>=0) {
+                linkedList.add(index,num);
+            } else {
+                linkedList.add(-(index+1),num);
+            }
+        }
+        length++;
+    }
+
+    public Double GetMedian() {
+        if (length%2!=0){
+            return Double.valueOf(linkedList.get((length-1)/2));
+        }else {
+            return (Double.valueOf(linkedList.get(length/2))+Double.valueOf(linkedList.get(length/2-1)))/2;
+        }
+    }
 }
+
+/*
+Leetcode拓展：
+        1.If all integer numbers from the stream are between 0 and 100, how would you optimize it?
+        2.If 99% of all integer numbers from the stream are between 0 and 100, how would you optimize it?
+*/
--- src/Coding_Interview/Sword_Offer_067/Solution.java
+++ src/Coding_Interview/Sword_Offer_067/Solution.java
@@ -1,20 +1,43 @@
 package Coding_Interview.Sword_Offer_067;
 
+import java.util.Map;
+
 /*
 给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1），
 每段绳子的长度记为k[0],k[1],...,k[m]。请问k[0]xk[1]x...xk[m]可能的最大乘积是多少？
 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
  */
 public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.cutRope(4));
+    }
     public int cutRope(int target) {
-        float mul=target/3;
-        int first=target/3;
-        int mid;
-        if (mul-first>first+1-mul){
-            mid=first+1;
-        }else{
-            mid=first;
+        int output = 0;
+        int[][] dp = new int[target][target];
+        for (int i=0;i<target;i++){
+            dp[0][i] = i+1;
         }
-        return 0;
+        for (int i=1;i<target;i++){
+            for (int j=0;j<target;j++){
+                if (j<i){
+                    dp[i][j] = -1;
+                }else if (j==i){
+                    dp[i][j] = 1;
+                }else{
+                    int max = 0;
+                    for (int k=1;k<=j-1;k++){
+                        if (dp[i-1][j-k]!=-1){
+                            max = Math.max(max,dp[i-1][j-k]*k);
+                        }
+                    }
+                    dp[i][j] = max;
+                }
+                if (j==target-1){
+                    output = Math.max(output,dp[i][j]);
+                }
+            }
+        }
+        return output;
     }
 }
