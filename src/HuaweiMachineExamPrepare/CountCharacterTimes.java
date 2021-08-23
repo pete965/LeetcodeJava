8fadb2e4d6270006ae4ecc96b1f5b264f1c4c5bb
sunboyang <sby2013@sina.com>
2021-08-23 21:38:04 +0800

    update 1 and 2

--- src/HuaweiMachineExamPrepare/CountCharacterTimes.java
+++ src/HuaweiMachineExamPrepare/CountCharacterTimes.java
@@ -1,2 +1,30 @@
-package HuaweiMachineExamPrepare;public class CountCharacterTimes {
+package HuaweiMachineExamPrepare;
+
+import java.util.Scanner;
+
+public class CountCharacterTimes {
+    public static void main(String[] args) {
+        int count =0;
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String find = sc.nextLine();
+        if (input.length()==0){
+            System.out.println(0);
+            return;
+        }
+        char findChar = find.charAt(0);
+        int anotherCase;
+        int dis = 'a' - 'A';
+        if (findChar > 'Z'){
+            anotherCase = findChar - dis;
+        }else {
+            anotherCase = findChar + dis;
+        }
+        for (int i=0;i<input.length();i++){
+            if (input.charAt(i)==findChar||input.charAt(i)==anotherCase){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
--- src/HuaweiMachineExamPrepare/LastWordLengthInAString.java
+++ src/HuaweiMachineExamPrepare/LastWordLengthInAString.java
@@ -1,2 +1,14 @@
-package HuaweiMachineExamPrepare;public class LastWordLengthInAString {
+package HuaweiMachineExamPrepare;
+
+import java.util.Scanner;
+
+public class LastWordLengthInAString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (sc.hasNext()) {
+            input = sc.next();
+        }
+        System.out.println(input.length());
+    }
 }
