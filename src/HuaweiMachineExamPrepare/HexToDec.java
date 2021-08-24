29d574c0e3b9c0a3c3d2aaeeb39550dc53055500
sunboyang <sby2013@sina.com>
2021-08-24 21:00:32 +0800

    update 4 and 5

--- src/HuaweiMachineExamPrepare/HexToDec.java
+++ src/HuaweiMachineExamPrepare/HexToDec.java
@@ -9,15 +9,15 @@ public class HexToDec {
             int dec = 0;
             int count = 0;
             String hex = sc.nextLine();
-            for (int i=hex.length()-1;i>=0;i--){
+            for (int i = hex.length() - 1; i >= 0; i--) {
                 char currentChar = hex.charAt(i);
-                if (currentChar == 'x'){
+                if (currentChar == 'x') {
                     break;
-                }else {
-                    if (currentChar>='0'&&currentChar<='9'){
-                        dec+=(currentChar-'0')*Math.pow(16,count);
-                    }else {
-                        dec+=(currentChar-'A'+10)*Math.pow(16,count);
+                } else {
+                    if (currentChar >= '0' && currentChar <= '9') {
+                        dec += (currentChar - '0') * Math.pow(16, count);
+                    } else {
+                        dec += (currentChar - 'A' + 10) * Math.pow(16, count);
                     }
                 }
                 count++;
---
+++ src/HuaweiMachineExamPrepare/SplitToPrimeNumbers.java
@@ -0,0 +1,35 @@
+package HuaweiMachineExamPrepare;
+
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class SplitToPrimeNumbers {
+    public static PriorityQueue<Long> priorityQueue = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        splitPrime(num);
+        while (!priorityQueue.isEmpty()) {
+            System.out.print(priorityQueue.poll() + " ");
+        }
+        System.out.print("\n");
+    }
+
+    private static void splitPrime(long num) {
+        /*
+        对于一个整数x,如果[2,sqrt(x)]范围内都没有可以被x整除的数，那么x就是质数；
+        因为如果x不是平方数，那么如果能分解成乘积，一定是一个大于sqrt(x)乘以一个小于sqrt(x)的数，所以后续的不用再试。
+         */
+        for (long i = 2; i < Math.sqrt((double) num); i++) {
+            if (num % i == 0) {
+                splitPrime(i);
+                splitPrime(num / i);
+                return;
+            }
+        }
+        priorityQueue.offer(num);
+    }
+
+}
